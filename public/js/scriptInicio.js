f3bf72608c5db6716c6f564448fe618c9d546de8
BuildTools <unconfigured@null.spigotmc.org>
2020-05-25 19:12:38 +0200

    Frases aleatorias

--- public/js/scriptInicio.js
+++ public/js/scriptInicio.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
+    let frases=new Array()
+    frases[0] = '"Desde 1988 trabajando por y para los conductores."'; 
+    frases[1] = '"Tu taller de confianza."'; 
+    frases[2] = '"Un grupo de profesionales"'; 
+    frases[3] = '"Estamos para arreglar tu vehiculo"'; 
+    frases[4] = '"Mejor taller, calidad - precio..."';
+
+    let x = frases.length; 
+    let whichFrase=Math.round(Math.random()*(x-1)); 
+    let fraseAle = document.getElementById('fraseAle');
+    fraseAle.innerHTML = frases[whichFrase];
+
     let boton1PopUp = document.getElementById('boton1');
 
     if (boton1PopUp) {
